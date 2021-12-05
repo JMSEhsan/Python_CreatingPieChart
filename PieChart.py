d3cc90d8a1d281cf4b4bae26dc25b8bc6374e626
Ehsan <JMSEhsan@gmail.com>
2021-12-05 00:02:05 -0700

    Commit

--- PieChart.py
+++ PieChart.py
@@ -1,6 +1,12 @@
 #  Creating Pie Chart
 # - W3schoosl.com
 
+
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,3 +14,7 @@ y = np.array([35, 25, 25, 15])
 
 plt.pie(y)
 plt.show() 
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
