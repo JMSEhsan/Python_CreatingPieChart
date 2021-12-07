7fa55e144ebfada00b97dd8e92055e359dacf781
Ehsan <JMSEhsan@gmail.com>
2021-12-07 00:04:39 -0700

    commit

--- Grid.py
+++ Grid.py
@@ -11,8 +11,8 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+x = np.array([1, 2, 3, 4, 5, 6, 6, 7, 20])
+y = np.array([4, 5, 10, 5, 12, 3, 15, 7, 7])
 
 plt.title("Sports Watch Data")
 plt.xlabel("Average Pulse")
